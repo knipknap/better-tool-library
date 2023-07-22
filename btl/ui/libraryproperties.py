17ae7554a1e14000792c0e5e835231c204000355
Samuel Abels <knipknap@gmail.com>
2023-07-22 19:15:01 +0200

    fix: library property editor not showing name

--- btl/ui/libraryproperties.py
+++ btl/ui/libraryproperties.py
@@ -13,11 +13,11 @@ class LibraryProperties(QtGui.QWidget):
         self.form = load_ui(ui_path)
         self.form.buttonBox.clicked.connect(self.form.reject)
         self.form.pushButtonSave.clicked.connect(self._on_save_clicked)
-        #self.form.lineEditLibraryName.setText(library.label)
+        self.form.lineEditLibraryName.setText(library.label)
         self.form.lineEditLibraryName.textChanged.connect(self._on_name_changed)
 
-        #if new:
-        #    self.form.pushButtonSave.setText("Create Library")
+        if new:
+            self.form.pushButtonSave.setText("Create Library")
 
         self.library = library
 
