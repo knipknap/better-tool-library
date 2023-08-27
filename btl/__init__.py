31cf1bbd2a559dcc8540037ae0dad8bcd7dc3329
Samuel Abels <knipknap@gmail.com>
2023-08-27 13:01:47 +0200

    include version number in __version__, if supported

--- btl/__init__.py
+++ btl/__init__.py
@@ -3,3 +3,13 @@ from .library import Library
 from .shape import Shape
 from .tool import Tool
 from .machine import Machine
+
+try:
+    from importlib.metadata import version, PackageNotFoundError
+except ImportError:
+    __version__ = "unknown; Python version too old"
+else:
+    try:
+        __version__ = version("btl")
+    except PackageNotFoundError:
+        __version__ = "unknown version"
