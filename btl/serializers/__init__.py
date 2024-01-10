2709f2aed6c0b5813b7969e2c4b30bfdd4e8421e
Alex Neufeld <alex.d.neufeld@gmail.com>
2024-01-10 14:32:38 -0700

    Add deserializer for fusion 360 tools

--- btl/serializers/__init__.py
+++ btl/serializers/__init__.py
@@ -1,9 +1,13 @@
 from .camoticsserializer import CamoticsSerializer
 from .fcserializer import FCSerializer
 from .linuxcncserializer import LinuxCNCSerializer
+from .fusionserializer import FusionToolsSerializer
+from .fusionserializer import FusionJSONSerializer
 
 serializers = {
     'camotics': CamoticsSerializer,
     'freecad': FCSerializer,
     'linuxcnc': LinuxCNCSerializer,
+    'fusion_tools': FusionToolsSerializer,
+    'fusion_json': FusionJSONSerializer,
 }
